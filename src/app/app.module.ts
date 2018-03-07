39870d5d2b163d09cc79e7aedd8fffd968a90991
Andres Ciceri <andresciceri@gmail.com>
2018-03-07 00:08:07 -0500

    feat: PWA favicons, manifest & main logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { Angulartics2Module } from "angulartics2";
+import { Angulartics2GoogleAnalytics } from "angulartics2/ga";
+import { environment } from '@env/environment';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
+    Angulartics2Module.forRoot([Angulartics2GoogleAnalytics])
   ],
   providers: [],
   bootstrap: [AppComponent]
